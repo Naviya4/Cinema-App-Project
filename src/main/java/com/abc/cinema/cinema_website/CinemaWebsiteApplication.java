ce282d50df98c47ebd2b2409df8ff06a710206aa
Naviya4 <4navindu@gmail.com>
2024-12-02 20:19:16 +0530

    Committed

---
+++ src/main/java/com/abc/cinema/cinema_website/CinemaWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.abc.cinema.cinema_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abc/cinema/cinema_website/CinemaWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.cinema.cinema_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
